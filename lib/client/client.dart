20d9009a5e3608daed6163d0dbb5f401a53366d1
Matthew Nitschke <matthewcnitschke@gmail.com>
2022-02-09 20:24:03 -0700

    fixed cross platform issue

--- lib/client/client.dart
+++ lib/client/client.dart
@@ -1,6 +1,7 @@
+
 import 'package:fi/client/client_interface.dart';
 
-// import 'ios_client_implementation.dart';
-import 'web_client_implementation.dart';
+import 'ios_client_implementation.dart'
+  if (dart.library.html) 'web_client_implementation.dart';
 
 FiClientInterface getClient() => FiClient();
